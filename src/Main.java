a4ec42909f851c3debbd76e685cbedb771abcb0c
MaxSpectik <126616558+MaxSpectik@users.noreply.github.com>
2023-03-15 18:10:04 +0300

    Homework7

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> vec1=  new Vector<String>(10);
-        List<String> arr1=  new ArrayList<String>(12);
-        ArrayDeque<Integer> que = new ArrayDeque<Integer>();
-        que.add(10);
-        que.add(12);
-        que.addFirst(1);
-        que.addLast(2);
-        System.out.println(que);
-        Set<Integer> set = new HashSet<Integer>();
-        Map<Integer,Integer> map1  = new HashMap<Integer,Integer>();
-        map1.put(1,3);
+        LoginTest MainTester = new LoginTest();
+        boolean TrueLogin = MainTester.isLoginValid("Dic_059");
+        if (TrueLogin) System.out.println("Логин соответствует требованиям");
+        else System.out.println("Логин не соответствует требованиям");
+
     }
     }
